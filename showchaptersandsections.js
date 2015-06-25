1d72a4a0645ee5ae7db4b964923795a099670edd
Jesse Anderson <jesse@smokinghand.com>
2015-06-25 12:24:27 -0700

    Changed macro to have to specify which chapter or section to highlight.

--- showchaptersandsections.js
+++ showchaptersandsections.js
@@ -1,11 +1,13 @@
 /*
 In the Markdown:
 
-![:showchapterorsection chapter]
-Will show all chapters and highlight the current chapter
+![:showchapters chaptername]
+Will show all chapters and highlight the chapter.
+The selected chapter will use the selected-chapter CSS class.
 
-![:showchapterorsection section]
-Will show all sections for the current chapter and highlight the current section
+![:showsections chaptername sectionname]
+Will show all sections for the chapter and highlight the section.
+The selected section will use the selected-section CSS class.
 */
 
 //ShowChapterOrSectionMacro.prototype.listOfChapters = []
@@ -14,8 +16,12 @@ Will show all sections for the current chapter and highlight the current section
 var ShowChapterOrSectionMacro = function (source) {
   var includeSourceMacro = this
 
-  remark.macros.showchapterorsection = function () {
-    return includeSourceMacro.addChapterOrSection(arguments[0]);
+  remark.macros.showchapters = function () {
+    return includeSourceMacro.outputChapters(arguments[0]);
+  };
+
+  remark.macros.showsections = function () {
+    return includeSourceMacro.outputSections(arguments[0],arguments[1]);
   };
 
   console.log("Added ShowChapterOrSection macros")
@@ -59,29 +65,44 @@ ShowChapterOrSectionMacro.prototype.parseSource = function(source) {
   }
 }
 
-// Adds a file by parsing the !INCLUDE
-ShowChapterOrSectionMacro.prototype.addChapterOrSection = function(chapterOrSection) {
-  if (chapterOrSection.toLowerCase() == "chapter") {
-    return this.outputChapters()
-  } else if (chapterOrSection.toLowerCase() == "section") {
-    // Output section information
-  } else {
-    console.log("Chapter Or Section argument not recognized. Argument was \"" 
-      + chapterOrSection + "\"")
-
-    return "Invalid Argument"
-  }
-};
-
-ShowChapterOrSectionMacro.prototype.outputChapters = function() {
+// Adds the text for the chapter macro
+ShowChapterOrSectionMacro.prototype.outputChapters = function(chapterName) {
   // Output chapter information
   chaptersList = "<ul>"
 
   for (var i = 0; i < this.listOfChapters.length; i++) {
-    chaptersList += "<li>" + this.listOfChapters[i] + "</li>"
+    chaptersList += "<li"
+
+    if (this.listOfChapters[i] == chapterName) {
+      chaptersList += " class=\"selected-chapter\""
+    }
+
+    chaptersList += ">" + this.listOfChapters[i] + "</li>"
   }
 
   chaptersList += "</ul>"
 
   return chaptersList
+};
+
+// Adds the text for the section macro
+ShowChapterOrSectionMacro.prototype.outputSections = function(chapterName, sectionName) {
+  // Output chapter information
+  chapterSectionsArray = this.chaptersToSectionsMap.get(chapterName)
+
+  sectionsList = "<ul>"
+
+  for (var i = 0; i < chapterSectionsArray.length; i++) {
+    sectionsList += "<li"
+
+    if (chapterSectionsArray[i] == sectionName) {
+      sectionsList += " class=\"selected-section\""
+    }
+
+    sectionsList += ">" + chapterSectionsArray[i] + "</li>"
+  }
+
+  sectionsList += "</ul>"
+
+  return sectionsList
 };
