97f65d5e171286bb440d204a5db043b3fe08110e
Jesse Anderson <jesse@smokinghand.com>
2015-07-16 18:07:49 -0700

    Added script to take a class and make a three level.

---
+++ slidestothreelevel.js
@@ -0,0 +1,63 @@
+var SlidesToThreeLevel = function () {
+};
+
+
+// Outputs timings and stats
+SlidesToThreeLevel.prototype.outputThreeLevel = function(slideshow, outputDiv) {
+  var slides = slideshow.getSlides()
+
+  output = ""
+
+  for (var i = 0; i < slides.length; i++) {
+    var template = slides[i].properties.template;
+    var slideName = slides[i].properties.name;
+
+    if (template == "chapter") {
+      output += slideName + "\n"
+    } else if (template == "section") {
+      output += "  " + slideName + "\n"
+    } else if (template == "regular") {
+      output += "    " + slideName + "\n"
+    } else if (template == "code") {
+      output += "    " + slideName + " #code\n"
+    } else if (template == "image") {
+      output += "    " + slideName + " #image\n"
+    } else if (template == "demo") {
+      var minutes = parseInt(slides[i].properties.minutes)
+      output += "    " + slideName + " #demo " + minutes + "\n"
+    } else if (template == "exercise") {
+      var minutes = parseInt(slides[i].properties.minutes)
+      output += "    " + slideName + " #exercise " + minutes + "\n"
+    }
+  }
+
+  outputDiv.html(output)
+}
+
+SlidesToThreeLevel.prototype.addHandler = function() {
+  $(document).ready(function () {
+
+    $('body').on('keyup',function(e){
+      if(e.which==84){
+        var slidesToThreeLevel = new SlidesToThreeLevel();
+        slidesToThreeLevel.outputThreeLevel(slideshow, $("#overlay"));
+        $('#overlay').toggle()
+      }
+    });
+
+    outputTextArea = $(document.createElement( "textarea" ))
+    outputTextArea.css({
+      "position":"absolute",
+      "top":0,
+      "left":0,
+      "width":"100%",
+      "height":"100%",
+      "z-index":1000,
+      "display":"none",
+    });
+   
+    outputTextArea.attr("id","overlay")
+
+    $("body").append(outputTextArea);
+  });
+}
