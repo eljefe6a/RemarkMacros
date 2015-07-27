c55a1ab058e44d1ad054a85a7ebaa157c5d96cf7
Jesse Anderson <jesse@smokinghand.com>
2015-07-27 11:45:17 -0700

    Refactoring. Added includemodule macro.

--- sourcecreator.js
+++ sourcecreator.js
@@ -1,3 +1,8 @@
+// Processes and includes all files in the class.json
+// 
+// Also processes the ![:includemodule] macro
+// Can be used with ![:includemodule path/to/module.md]
+
 var SourceCreator = function () {
 };
 
@@ -22,19 +27,10 @@ SourceCreator.prototype.createSource = function(classJSONUrl, showChapters) {
     fileSource = this.getFile(classJSON.classmodules[i]);
 
     if (showChapters == true) {
-      // Add the chapters macro
-      var m;
-      var re = /template:\s+chapter\nname:\s+(.*)\n/;
- 
-      if ((m = re.exec(fileSource)) !== null) {
-        source += "\ntemplate: chapterorsectionlist\n"
-        source += "name: Course Chapters\n"
-        source += "![:showchapters " + m[1] + "]\n"
-        source += "---\n"
-      }
+      source += this.addChapterOrSectionList(fileSource)
     }
 
-    source += fileSource
+    source += this.importModule(fileSource, classJSON.classmodules[i])
 
     // Files shouldn't have --- at the head or foot
     // It is added automatically here
@@ -44,6 +40,39 @@ SourceCreator.prototype.createSource = function(classJSONUrl, showChapters) {
   };
 
   return source
+}
+
+SourceCreator.prototype.importModule = function(fileSource, moduleFileName) {
+  // Import any includemodule
+  var m;
+  var re = /!\[:includemodule (.*)\]\n/;
+
+  // Keep on running Regex until all includemodule are replaced
+  // with their module contents
+  while ((m = re.exec(fileSource)) !== null) {
+    fileSource = fileSource.replace(m[0], this.getFile(m[1]))
+
+    console.log("Importing module: " + m[1] + " for " + moduleFileName)
+  }
+
+  return fileSource
+}
+
+SourceCreator.prototype.addChapterOrSectionList = function(fileSource) {
+  // Add the chapters macro
+  var m;
+  var re = /template:\s+chapter\nname:\s+(.*)\n/;
+
+  if ((m = re.exec(fileSource)) !== null) {
+    source = "\ntemplate: chapterorsectionlist\n"
+    source += "name: Course Chapters\n"
+    source += "![:showchapters " + m[1] + "]\n"
+    source += "---\n"
+
+    return source;
+  }
+
+  return ""
 }
 
 SourceCreator.prototype.getFile = function(url) {
